2c46de011086a755d885dba821d952499889fdd4
jshapi4 <jshapi4@wgu.edu>
2024-08-08 21:58:17 -0400

    exercise 4.4 tests

---
+++ part4/blog-list/utils/list_helper.js
@@ -0,0 +1,14 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((accumulator, blog) => {
+    return accumulator + blog.likes
+  }, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+}
