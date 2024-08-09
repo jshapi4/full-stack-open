5d458615c3581d619afb5dbedecce532e383c7fb
jshapi4 <jshapi4@wgu.edu>
2024-08-09 11:09:05 -0400

    exercise 4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -8,7 +8,24 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+function findMostLikedBlog(blogs) {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  let mostLikedBlog = blogs[0]
+
+  for (let i = 1; i < blogs.length; i++) {
+    if (blogs[i].likes > mostLikedBlog.likes) {
+      mostLikedBlog = blogs[i] // Update if the current blog has more likes
+    }
+  }
+
+  return mostLikedBlog
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  findMostLikedBlog,
 }
