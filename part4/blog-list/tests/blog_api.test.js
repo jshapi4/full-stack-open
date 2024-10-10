7454c5b4f3146abb4115b07735d8d055fc8f506e
jshapi4 <jshapi4@wgu.edu>
2024-10-09 23:02:17 -0400

    exercise 4.8

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -32,12 +32,19 @@ beforeEach(async () => {
   }
 })
 
-test.only('blogs are returned as json', async () => {
-  console.log('entered test')
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+// exercse 4.8 verify the blog list application returns the correct amount of blog posts in the JSON format
+
+test('correct amount of blog posts are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 test('there are two blogs', async () => {
